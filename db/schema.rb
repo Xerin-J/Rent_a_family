ef20b26f9242aae93b06a60bbd0b1045b09fc071
Aerin Jang <205189969+Xerin-J@users.noreply.github.com>
2025-07-22 20:29:40 +0900

    renamed password to password digest for secured password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_19_081042) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_22_112431) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,7 +42,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_19_081042) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
