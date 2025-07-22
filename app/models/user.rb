7b622f649ed9b293004f56a53ba431f1717b96ae
Aerin Jang <205189969+Xerin-J@users.noreply.github.com>
2025-07-22 20:21:14 +0900

    updated user model with validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   has_many :families
   has_many :bookings
 
-  validates :username, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :password, length: { minimum: 8 }, if: -> { new_record? || !password.nil? }
 end
