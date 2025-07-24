d505efdb4b5368604969486b49190d0f86f7b5da
Aerin Jang <205189969+Xerin-J@users.noreply.github.com>
2025-07-24 21:38:56 +0900

    Updated

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,8 +1,8 @@
 class Family < ApplicationRecord
-  belongs_to :user
-  has_many :bookings
+  # belongs_to :user
+  # has_many :bookings
 
-  validates :members, presence: true, length: { minimum: 1, maximum: 20 }
+  validates :members, presence: true, length: { minimum: 1, maximum: 100 }
   validates :location, presence: true
   validates :hourly_rate, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :event_type, presence: true
