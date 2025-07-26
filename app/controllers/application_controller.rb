a5bc7e4f8e2dc418a7cc6ab0c0f8fdf5dc29fe90
Antonio Vinciguerra <anto.vinciguerra@hotmail.com>
2025-07-26 12:56:05 +0900

    updating user page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   # This will redirect users after login based on their role
- def after_sign_in_path_for(resource)
-  if resource.role == "provider"
-    if resource.provider.present?
-      providers_path  # provider dashboard
+  def after_sign_in_path_for(resource)
+    if resource.role == "provider"
+      providers_path  # provider dashboard path
     else
-      new_provider_path  # guide them to create their Provider profile
+      root_path       # regular user landing page
     end
-  else
-    root_path  # normal users go here
   end
-end
 
     protected
 
