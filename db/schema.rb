96c31b0b08c9313e7daa2a0f777639ed90ab0763
Antonio Vinciguerra <anto.vinciguerra@hotmail.com>
2025-07-28 15:48:56 +0900

    addedd details for the samilies and moved it to homepage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_28_030418) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_28_063258) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_28_030418) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "provider_id"
+    t.float "rating"
     t.index ["provider_id"], name: "index_families_on_provider_id"
     t.index ["user_id"], name: "index_families_on_user_id"
   end
